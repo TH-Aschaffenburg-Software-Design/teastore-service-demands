a695b86cbab3977085de65ed4e013fcb56c07e90
ElijahSey <seyfarthelijah2002@gmail.com>
2025-08-03 16:02:58 +0200

    Add space trimming

--- load_generation_machine/scripts/get_service_ip.sh
+++ load_generation_machine/scripts/get_service_ip.sh
@@ -2,7 +2,7 @@
 
 namespace=teastore
 
-ip=$(properties/get_property.sh kubernetes.master.ip)
-user=$(properties/get_property.sh kubernetes.master.user)
+ip=$(properties/get_property.sh kubernetes.master.ip | tr -d '[:space:]')
+user=$(properties/get_property.sh kubernetes.master.user | tr -d '[:space:]')
 
 ssh $user@$ip "kubectl get pods -n $namespace -o wide --field-selector='status.phase==Running' | grep 'teastore-$1' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'"
