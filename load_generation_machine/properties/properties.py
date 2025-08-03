a8b8eb41bc0cd8a9673674209f33ed1ceabfa6ee
ElijahSey <seyfarthelijah2002@gmail.com>
2025-08-03 20:28:36 +0200

    Strip newlines

--- load_generation_machine/properties/properties.py
+++ load_generation_machine/properties/properties.py
@@ -2,4 +2,4 @@ import subprocess
 
 
 def get_property(key: str):
-    return subprocess.check_output(["properties/get_property.sh", key]).decode()
+    return subprocess.check_output(["properties/get_property.sh", key]).decode().strip()
