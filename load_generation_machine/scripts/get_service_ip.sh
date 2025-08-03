9a1cd9f63f10ab99c7e619feb2eddcbd1551b14a
ElijahSey <seyfarthelijah2002@gmail.com>
2025-08-03 15:30:53 +0200

    Correct brackets

--- load_generation_machine/scripts/get_service_ip.sh
+++ load_generation_machine/scripts/get_service_ip.sh
@@ -1,6 +1,6 @@
 namespace=teastore
 
-ip=${properties/get_property.sh kubernetes.master.ip}
-user=${properties/get_property.sh kubernetes.master.user}
+ip=$(properties/get_property.sh kubernetes.master.ip)
+user=$(properties/get_property.sh kubernetes.master.user)
 
 ssh $user@$ip "kubectl get pods -n $namespace -o wide --field-selector='status.phase==Running' | grep 'teastore-$1' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}'"
